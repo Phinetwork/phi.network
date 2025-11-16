53983882976ec77bc9df4a69acdbf56ddba43084
Kojib <kojibchat@gmail.com>
2025-11-16 03:07:24 -0500

    v2.6

--- src/components/VerifierStamper/index.tsx
+++
@@ -1,8 +0,0 @@
-// Aggregator: exports the component and verification helpers
-
-import VerifierStamper from "./VerifierStamper";
-
-export { verifyHistorical } from "./verifyHistorical";
-export { verifySovereignOffline, type SovereignVerifyReport } from "./verifySovereignOffline";
-
-export default VerifierStamper;
